acd2d0b0d2059411518a9143ed4270a2a9be8f9d
MrAkmal <mr.akmal4555@mail.ru>
2022-10-31 13:10:04 +0500

    first commit

---
+++ src/test/java/com/example/round_assignment/RoundAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.round_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoundAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
